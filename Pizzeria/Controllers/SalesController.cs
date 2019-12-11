cac643a1eb16005a5243d364617576a5a9efe61f
Kamil Bielecki <s16951@pjwstk.edu.pl>
2019-12-11 16:45:14 +0100

    Ćwiczenia PRO (dodanie delete, put, post do sales)

--- Pizzeria/Controllers/SalesController.cs
+++ Pizzeria/Controllers/SalesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Pizzeria.Models;
 
 namespace Pizzeria.Controllers
@@ -12,7 +13,7 @@ namespace Pizzeria.Controllers
     [ApiController]
     public class SalesController : ControllerBase
     {
-        private s16951Context _context;
+        private readonly s16951Context _context;
         public SalesController(s16951Context context)
         {
             _context = context;
@@ -24,15 +25,57 @@ namespace Pizzeria.Controllers
             return Ok(_context.Promocja.ToList());
         }
 
-        [HttpGet("{id:int}")]
-        public IActionResult GetSale(int id)
+        [HttpGet("{IdPromocja:int}")]
+        public IActionResult GetSale(int IdPromocja)
         {
 
-            var sale = _context.Promocja.FirstOrDefault(e => e.IdPromocja == id);
+            var sale = _context.Promocja.FirstOrDefault(e => e.IdPromocja == IdPromocja);
             if (sale == null)
                 return NotFound();
 
             return Ok(sale);
         }
+
+        [HttpPost]
+        public IActionResult Create(Promocja newSale)
+        {
+            _context.Promocja.Add(newSale);
+            _context.SaveChanges();
+
+
+            return StatusCode(201, newSale);
+        }
+
+        [HttpPut("{IdPromocja:int}")]
+        public IActionResult Update(int IdPromocja,Promocja updatedSale)
+        {
+            if(_context.Promocja.Count(e => e.IdPromocja == IdPromocja)==0)
+            {
+                return NotFound();
+            }
+
+            _context.Promocja.Attach(updatedSale);
+            _context.Entry(updatedSale).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(updatedSale);
+        }
+
+
+          [HttpDelete("{IdPromocja:int}")]
+        public IActionResult Delete(int IdPromocja)
+        {
+            var sale = _context.Promocja.FirstOrDefault(e => e.IdPromocja == IdPromocja);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+
+            _context.Promocja.Remove(sale);
+            _context.SaveChanges();
+
+            return Ok(sale);
+        }
+        
     }
 }
